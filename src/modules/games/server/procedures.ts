b02173f6d5763b033c62cac1df24afcfdea24bf6
myk <michaelmugendi1707@gmail.com>
2025-08-27 16:11:23 +0300

    Update: pull published games

--- src/modules/games/server/procedures.ts
+++ src/modules/games/server/procedures.ts
@@ -21,7 +21,7 @@ export const gamesRouter = createTRPCRouter({
   getMany: baseProcedure.query(async () => {
     const games = await prisma.game.findMany({
       where: {
-        published: false, // Only fetch published games
+        published: true, // Only fetch published games
       },
       orderBy: {
         updatedAt: "asc",
